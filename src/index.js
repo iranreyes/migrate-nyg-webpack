5b4719f437a86f145cb7e98142a19dc2ebad59fa
Iran Reyes <iran.reyes@jam3.com>
2017-10-24 02:41:50 -0300

    Added stats.js #1

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV === 'development') require('./util/stats')();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
